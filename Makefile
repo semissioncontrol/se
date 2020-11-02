fa97fb797f7028021a9bb61a28036696c14361a4
Bobbbay <49123594+Bobbbay@users.noreply.github.com>
2020-11-02 15:27:38 -0500

    Updated code
    Signed-off-by: Bobbbay <49123594+Bobbbay@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all:
 	@rm -rf build; mkdir build; cd build; \
 	cmake ..; \
 	make
+
+devel:
+	cd build; make;
--- main.c
+++ main.c
@@ -2,5 +2,10 @@
 
 int main(int argc, char** argv) {
     printf("%s", argv[0]);
+    if (argv[1] == "bak") {
+        printf("bak\n");
+    } else {
+        printf("%s: command not found: %s\n", argv[0], argv[1]);
+    }
     return 0;
 }
